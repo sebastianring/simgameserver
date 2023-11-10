f900d301f2df563b54abd1bd710a0d3333ae5cb0
sebastian <sebastianring@gmail.com>
2023-11-10 12:50:42 +0100

    Fixed local db connection

--- db/db.go
+++ db/db.go
@@ -16,15 +16,25 @@ type DBboard struct {
 }
 
 func OpenDbConnection() (*sql.DB, error) {
+
 	prefix := "postgres://"
 	user := "sim_game"
 	password := os.Getenv("SIM_GAME_DB_PW")
-	adress := "5.150.233.156"
+	fmt.Println("Password:", password)
+	adress, hit := os.LookupEnv("SIM_GAME_DB_IP")
+
+	if !hit {
+		fmt.Println("No environment variable set:", adress)
+		adress = "5.150.233.156"
+	}
+
 	port := "5432"
 
 	database_url := prefix + user + ":" +
 		password + "@" + adress + ":" +
 		port + "/postgres"
+
+	fmt.Println("Trying to connect to DB: ", database_url)
 
 	db, err := sql.Open("postgres", database_url)
 
@@ -32,20 +42,20 @@ func OpenDbConnection() (*sql.DB, error) {
 		fmt.Println(err.Error())
 
 		return nil, err
+
 	} else {
-		fmt.Println("Database connection secured!")
+		fmt.Println("Database connection secured")
 	}
 
+	fmt.Println("Pinging database")
 	err = db.Ping()
 
 	if err != nil {
 		fmt.Println(err.Error())
-
 		return nil, err
-
-	} else {
-		fmt.Println("Database pinged!")
 	}
+
+	fmt.Println("Database pinged!")
 
 	return db, nil
 }
